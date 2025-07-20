a76ce84afa0f713a3805a18eb7fb1634985946cd
ChrisKincaid <christophergkincaid@msn.com>
2025-07-20 14:31:07 -0500

    Add post-card and subscription form components with styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { CategoryNavbar } from './layout/category-navbar/category-navbar';
 import { Footer } from './layout/footer/footer';
+import { PostCard } from './layout/post-card/post-card';
+import { SubscriptionForm } from './subscription-form/subscription-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, CategoryNavbar, Footer],
+  imports: [
+    RouterOutlet, 
+    Header, 
+    CategoryNavbar, 
+    Footer, 
+    PostCard, 
+    SubscriptionForm
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
