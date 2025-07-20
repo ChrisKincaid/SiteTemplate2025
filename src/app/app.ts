3ee3de6475fce72018e471985439f9bab72ac42d
ChrisKincaid <christophergkincaid@msn.com>
2025-07-20 13:12:26 -0500

    Add footer component and update layout structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
+import { CategoryNavbar } from './layout/category-navbar/category-navbar';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, CategoryNavbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
