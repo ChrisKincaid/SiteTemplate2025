a0c4fc59f208daae5717c91a6710f563afe935d5
ChrisKincaid <christophergkincaid@msn.com>
2025-07-24 16:52:04 -0500

    Fix CategoryNavbar for multiple categories - dynamic height and responsive layout

--- src/app/layout/category-navbar/category-navbar.ts
+++ src/app/layout/category-navbar/category-navbar.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Categories } from '../../services/categories';
 
 @Component({
   selector: 'app-category-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './category-navbar.html',
   styleUrl: './category-navbar.css'
 })
-export class CategoryNavbar {
+export class CategoryNavbar implements OnInit {
+
+  categoryArray: Array<any> = [];
+
+  constructor( 
+    private categoryService: Categories,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    console.log('CategoryNavbar ngOnInit called');
+    this.categoryService.loadData().subscribe({
+      next: (val) => {
+        console.log('Categories loaded:', val);
+        console.log('Number of categories:', val?.length);
+        console.log('First category structure:', JSON.stringify(val[0], null, 2));
+        this.categoryArray = val;
+        console.log('categoryArray after assignment:', this.categoryArray);
+        // Force change detection
+        this.cdr.detectChanges();
+        console.log('Change detection triggered');
+      },
+      error: (error) => {
+        console.error('Error loading categories:', error);
+      }
+    });
+  }
+
+  trackByFn(index: number, item: any) {
+    return item.id;
+  }
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAVXF3hZ5JThY8_EjpSEcBV0wf4xbnPO9Q",
+    authDomain: "sitetemplate2025-admin.firebaseapp.com",
+    projectId: "sitetemplate2025-admin",
+    storageBucket: "sitetemplate2025-admin.firebasestorage.app",
+    messagingSenderId: "917850920814",
+    appId: "1:917850920814:web:1bb4dc5434e15128c3137f",
+    measurementId: "G-FKRDJ1B5ST"
+  }
+};
