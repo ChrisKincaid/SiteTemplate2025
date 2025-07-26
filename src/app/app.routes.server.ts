f0693515d3c1ab612d3a78715e651b79865256f9
ChrisKincaid <christophergkincaid@msn.com>
2025-07-26 13:19:11 -0500

    code build error fixes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,21 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+// Define only static routes for prerendering - exclude dynamic routes with parameters
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'terms',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'contact',
     renderMode: RenderMode.Prerender
   }
 ];
